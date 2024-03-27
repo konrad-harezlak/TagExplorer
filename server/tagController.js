6a98f5c21b4b48057e5989b72fe6c566aabc3d7b
konrad-harezlak <konrad.harezlakk@gmail.com>
2024-03-27 14:47:52 +0100

    Add css files

--- server/tagController.js
+++ server/tagController.js
@@ -3,6 +3,9 @@ const fetchTags = async (req, res) => {
   
     try{
         const response = await fetch(`https://api.stackexchange.com/2.3/tags?pagesize=${pageSize}&order=${order}&sort=${sort}&site=stackoverflow`)
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
         const data = await response.json();
         res.status(200).json(data)
     }catch(err){
